aeb64216ae72474a4ce2636dc2b45d234f6459ce
Harshad Thombare <thombareharshad@gmail.com>
2025-01-21 19:30:07 +0100

    Committing code for ViewChildren operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,43 @@
-import { Component, EventEmitter } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, QueryList, ViewChildren } from '@angular/core';
 import {COURSES} from '../db-data';
 import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
 
-  courses = [...COURSES];
+  courses = COURSES;
 
-  startDate = new Date(2024, 11, 11);
+  @ViewChildren(CourseCardComponent)
+  cards:QueryList<CourseCardComponent>;
 
-  title = COURSES[0].description;
-
-  price = 9.99001145;
-
-  rate = 0.67;
-
-  course = COURSES[0];
+  ngAfterViewInit(){
+    this.cards.changes.subscribe(
+      cards => console.log(cards)
+    );
+  }
 
   onCourseSelected(course:Course) {
-    console.log("App component - click event bubbled...", course);
+  }
+
+  onCoursesEdited() {
+    this.courses.push(
+      {
+        id: 1,
+        description: "Angular core deep dive",
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+        category: 'INTERMEDIATE',
+        lessonsCount: 10
+      } 
+    );
   }
 
   trackCourse(index:number, course:Course) {
-    return course.id;
   }
 
 }
