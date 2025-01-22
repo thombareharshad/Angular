e2d9bba68a58256d89786b611a493282f3c133a7
Harshad Thombare <thombareharshad@gmail.com>
2025-01-22 21:55:42 +0100

    Committing code for ContentChild with ng content module

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,6 +1,7 @@
 import { Course } from './../model/course';
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output, ViewChild, OnInit, AfterViewInit, ContentChild, ElementRef } from '@angular/core';
 import { NgIf, NgClass, NgStyle, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { CourseImageComponent } from '../course-image/course-image.component';
 
 @Component({
   selector: 'course-card',
@@ -10,16 +11,27 @@ import { NgIf, NgClass, NgStyle, NgSwitch, NgSwitchCase, NgSwitchDefault } from 
   styleUrl: './course-card.component.css'
 })
 
-export class CourseCardComponent {
-
+export class CourseCardComponent implements OnInit, AfterViewInit {
+  
   @Input()
   course:Course;
+
+  @ContentChild(CourseImageComponent, {read: ElementRef})
+  image:ElementRef;
 
   @Output('courseSelected')
   courseEmitter = new EventEmitter<Course>();
 
+  ngAfterViewInit(): void {
+    console.log(this.image);
+  }
+
+  ngOnInit(): void {
+    
+  }
+
   onCourseViewed() {
-    console.log("Card Component - button clicked ...");
+    // console.log("Card Component - button clicked ...");
 
     this.courseEmitter.emit(this.course);
   }
