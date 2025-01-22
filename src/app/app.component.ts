2f125c4076d0f2fa1ee3bc5dc1dfd5b82e6bafbc
Harshad Thombare <thombareharshad@gmail.com>
2025-01-22 20:07:23 +0100

    Committing code for <ng-content> tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,8 @@ export class AppComponent {
 
   courses = [...COURSES];
 
-  startDate = new Date(2024, 11, 11);
-
-  title = COURSES[0].description;
-
-  price = 9.99001145;
-
-  rate = 0.67;
-
-  course = COURSES[0];
-
   onCourseSelected(course:Course) {
-    console.log("App component - click event bubbled...", course);
+    // console.log("App component - click event bubbled...", course);
   }
 
   trackCourse(index:number, course:Course) {
