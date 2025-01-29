84ee1919a3a4db312f2343965709b02459cd0bc3
Harshad Thombare <thombareharshad@gmail.com>
2025-01-29 22:43:22 +0100

    Committing depedency files

--- node_modules/@angular/build/src/tools/sass/lexer.d.ts
+++ node_modules/@angular/build/src/tools/sass/lexer.d.ts
@@ -3,7 +3,7 @@
  * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * found in the LICENSE file at https://angular.dev/license
  */
 /**
  * Scans a CSS or Sass file and locates all valid url function values as defined by the
--- node_modules/@angular/build/src/utils/delete-output-dir.d.ts
+++ node_modules/@angular/build/src/utils/delete-output-dir.d.ts
@@ -3,7 +3,7 @@
  * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * found in the LICENSE file at https://angular.dev/license
  */
 /**
  * Delete an output directory, but error out if it's the root of the project.
